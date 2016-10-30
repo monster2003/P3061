0df58996cc58e5f61aa17db2bf5e3d1242bd4384
monster2003 <v.alex.4mail@gmail.com>
2016-10-30 13:10:23 +0300

    edit

--- P3061.pas
+++ P3061.pas
@@ -4,5 +4,16 @@
  * Date: 30.10.2016
  *)
 program P3061;
+var
+    N,k,i:Integer ;
 begin
+    ReadLn(N);
+    i:=1;
+    k:=0;
+    while i<N do
+    begin
+        i:=2*i;
+        k:=k+1;
+    end;
+    WriteLn(k);
 end.
