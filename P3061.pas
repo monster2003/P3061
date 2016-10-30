069b958b5e94f00062bdfdb2acfc6e041fe44640
monster2003 <v.alex.4mail@gmail.com>
2016-10-30 12:17:51 +0300

    Initial commit

---
+++ P3061.pas
@@ -0,0 +1,18 @@
+(*
+ * Project: P3061
+ * User: ???????????
+ * Date: 30.10.2016
+ *)
+program P3061;
+var
+    N,k:Integer ;
+begin
+    ReadLn(N);
+    k:=1;
+    while k>=N do
+    begin
+        k:=2*2;
+    end;
+    WriteLn(k);
+
+end.
